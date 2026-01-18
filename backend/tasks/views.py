56f66e505533519f258460c4db36905cf879d417
Emman Aguilar <aguilaremman3@gmail.com>
2026-01-18 12:02:19 +0800

    creating endpoints with viewset

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ViewSet):
+    
+    # GET /tasks/
+    def list(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    # POST /tasks/
+    def create(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # GET /tasks/{id}/
+    def retrieve(self, request, pk=None):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    # PUT /tasks/{id}/
+    def update(self, request, pk=None):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # PATCH /tasks/{id}/ - Toggle completed status
+    def partial_update(self, request, pk=None):
+        task = get_object_or_404(Task, pk=pk)
+        # Toggle the boolean value
+        task.completed = not task.completed
+        task.save()
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    # DELETE /tasks/{id}/
+    def destroy(self, request, pk=None):
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
